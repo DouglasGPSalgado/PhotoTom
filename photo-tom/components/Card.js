86d8cc1a2972f74e034bc7d6788e25f8c84148c9
Matheus Saito <matheussaito95k@gmail.com>
2023-02-03 16:34:33 -0300

    Troca pagina Home e Card

--- photo-tom/components/Card.js
+++ photo-tom/components/Card.js
@@ -1,98 +1,110 @@
 import { React, useState } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
-import { globalStyles } from "../GloblaStyles";
+import {
+    TouchableOpacity
+} from "react-native";
+import {
+    NativeBaseProvider,
+    Box,
+    Text,
+} from "native-base";
 import { StyleSheet } from "react-native";
-import { useNavigation } from "@react-navigation/native";
 
 function Card(props) {
-  const [detalhes, setDetalhes] = useState(false);
-  const navigation = useNavigation();
 
-  return (
-    <View style={styles.card}>
-      <View style={globalStyles.linha}>
-        <View style={styles.descricao}>
-          <Text style={styles.titulo}>{props.titulo}</Text>
-          <Text style={styles.subTitulo}>{props.subTitulo}</Text>
-        </View>
-        <View style={styles.botaoDescricao}>
-          <TouchableOpacity
-            style={styles.detalhes}
-            onPress={(detalhes) => setDetalhes(true)}
-          >
-            <Text style={styles.detalhesText}>?</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-      {detalhes == true ? (
-        <Text style={styles.descricao}>{props.descricao}</Text>
-      ) : (
-        <></>
-      )}
-      <TouchableOpacity onPress={navigation.navigate("Palette")}>
-        <Text style={styles.acaoPrimaria}>{props.acaoPrimaria}</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
-export default Card;
+    const [detalhes, setDetalhes] = useState(false);
+    return (
+
+        <NativeBaseProvider>
+            <Box
+                width="90%"
+                margin="4"
+                padding="4"
+                alignSelf="center"
+                alignItems="center"
+                justifyContent="center"
+                bg="#FFFFFF"
+                borderRadius="8"
+                shadow="7"
+            >
+                <Box
+                    flexDirection="row"
+                >
+                    <Box
+                        alignItems="center"
+                        justifyContent="center"
+                        width="90%"
+                    >
+                        <Text
+                            textAlign="center"
+                            fontSize="18"
+                            fontWeight="bold"
+                            color="#003E52"
+                        >
+                            {props.titulo}
+                        </Text>
+                    </Box>
+                    <Box
+                        width="15%"
+                        alignItems="center"
+                    >
+                        <TouchableOpacity
+                            style={styles.detalhes}
+                            onPress={(detalhes) => setDetalhes(true)}
+                        >
+                            <Text
+                                fontSize="16"
+                                fontWeight="bold"
+                                color="#FFFFFF"
+                            >
+                                ?
+                            </Text>
+                        </TouchableOpacity>
+                    </Box>
+                </Box>
+                <Text
+                    fontSize="12"
+                    fontWeight="normal"
+                    color="#003E52"
+                >
+                    {props.subTitulo}
+                </Text>
+                {
+                    (detalhes == true) ?
+                        <Text
+                            textAlign="center"
+                            fontSize="16"
+                            fontWeight="medium"
+                            color="#003E52"
+                        >
+                            {props.descricao}
+                        </Text>
+                        : <>
+                        </>
+                }
+                <TouchableOpacity>
+                    <Text
+                        margin="2"
+                        fontSize="20"
+                        color="#00B707"
+                    >
+                        {props.acaoPrimaria}
+                    </Text>
+                </TouchableOpacity>
+            </Box>
+        </NativeBaseProvider >
+    );
+
+} export default Card;
 
 const styles = StyleSheet.create({
-  card: {
-    alignItems: "center",
-    margin: 30,
-    padding: 10,
-    minHeight: 100,
-    borderRadius: 15,
-    backgroundColor: "#FFFFFF",
-  },
 
-  descricao: {
-    width: "85%",
-    alignItems: "center",
-    paddingStart: "15%",
-  },
-  botaoDescricao: {
-    width: "15%",
-    alignItems: "center",
-  },
+    detalhes: {
+        width: 24,
+        height: 24,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 20,
+        backgroundColor: "#003E52",
+    },
 
-  detalhes: {
-    width: 24,
-    height: 24,
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 20,
-    backgroundColor: "#003E52",
-  },
-  detalhesText: {
-    fontSize: 18,
-    fontWeight: "bold",
-    color: "#FFFFFF",
-  },
-
-  titulo: {
-    fontSize: 16,
-    fontWeight: "bold",
-    color: "#003E52",
-    textAlign: "center",
-  },
-  subTitulo: {
-    fontSize: 10,
-    fontWeight: "400",
-    color: "#003E52",
-    textAlign: "center",
-  },
-  descricao: {
-    fontSize: 14,
-    margin: 4,
-    fontWeight: "500",
-    color: "#003E52",
-    textAlign: "center",
-  },
-  acaoPrimaria: {
-    margin: 20,
-    fontSize: 20,
-    color: "#00B707",
-  },
 });
--- photo-tom/pages/Home.js
+++ photo-tom/pages/Home.js
@@ -1,66 +1,93 @@
 import { React } from "react";
 import {
-  View,
   ImageBackground,
   Image,
   StyleSheet,
   ScrollView,
+  SafeAreaView,
 } from "react-native";
+import { NativeBaseProvider, Box } from "native-base";
 
 import Card from "../components/Card";
 import GradientText from "../components/GradientText";
 
+import Form from "../components/FormComponent";
+
 export default function Home({ navigation }) {
   return (
-    <ScrollView style={{ flex: 1, flexDirection: "column" }}>
-      <ImageBackground
-        source={require("../assets/Background.png")}
-        style={{ flex: 1 }}
-        resizeMode="cover"
-      >
-        <View style={{ alignItems: "center" }}>
-          <Image
-            source={require("../assets/image_home.png")}
-            style={{ height: 200, width: 200, alignSelf: "center" }}
-            resizeMode="contain"
-          />
-          <GradientText
-            style={styles.title}
-            text={"Escolha o tipo de análise"}
-          />
-          <GradientText
-            style={styles.info}
-            text={"Caso tenha duvidas clique no botão “?” para mais detalhes"}
-          />
-        </View>
-        <View style={{ paddingVertical: "5%" }}>
-          <Card
-            titulo={"PALETA DE CORES"}
-            subTitulo={"Comparação visual do técnico"}
-            descricao={
-              "Análise simples onde o técnico tira uma foto do cliente e compara visualmente com uma paleta de cores + Formulário para análise minuciosa"
-            }
-            acaoPrimaria={"Continuar"}
-          />
-          <Card
-            titulo={"Reconhecimento de Imagem"}
-            subTitulo={"Visão computacional"}
-            descricao={
-              "Reconhecimento de fototipo visual automática a partir de foto do local do procedimento + Formulário para análise minuciosa"
-            }
-            acaoPrimaria={"Continuar"}
-          />
-        </View>
-      </ImageBackground>
-    </ScrollView>
+    <NativeBaseProvider>
+      <ScrollView style={{ flex: 1, flexDirection: "column" }}>
+        <ImageBackground
+          source={require("../assets/Background.png")}
+          style={{ flex: 1 }}
+          resizeMode="cover"
+        >
+          {/* <View
+            flexDirection={"row"}
+          >
+            <Text>Teste</Text>
+          </View> */}
+          <Box
+            //Viel trocada por Box na Native Base
+            alignItems="center"
+          >
+            <Image
+              source={require("../assets/image_home.png")}
+              style={{ height: 200, width: 200, alignSelf: "center" }}
+              resizeMode="contain"
+            />
+            <GradientText
+              style={styles.title}
+              text={"Escolha o tipo de análise"}
+            />
+            <GradientText
+              style={styles.info}
+              text={"Caso tenha duvidas clique no botão “?” para mais detalhes"}
+            />
+          </Box>
+          <Box
+            px="5%" //px = paddingVertical
+          >
+
+            <Form 
+              titulo={"Qual a cor da pele do paciente antes da exposição solar?"}
+              subTitulo={""}
+              A={"Branco Marfim"}
+              B={"Pele Clara ou Pálida"}
+              C={"Pele com Tom de Ouro"}
+              D={"Castanho Claro"}
+              E={"Castanho Escuro ou Preto"}
+            />
+            <Card
+              titulo={"Paleta de Cores"}
+              subTitulo={"Comparação visual do técnico"}
+              descricao={
+                "Análise simples onde o técnico tira uma foto do cliente e compara visualmente com uma paleta de cores + Formulário para análise minuciosa"
+              }
+              acaoPrimaria={"Continuar"}
+            />
+            <Card
+              titulo={"Reconhecimento de Imagem"}
+              subTitulo={"Visão computacional"}
+              descricao={
+                "Reconhecimento de fototipo visual automática a partir de foto do local do procedimento + Formulário para análise minuciosa"
+              }
+              acaoPrimaria={"Continuar"}
+            />
+          </Box>
+        </ImageBackground>
+      </ScrollView>
+    </NativeBaseProvider>
   );
 }
 const styles = StyleSheet.create({
   title: {
+    marginHorizontal: 10,
     fontSize: 32,
     textAlign: "left",
   },
   info: {
+    marginHorizontal: 10,
     fontSize: 10,
     color: "#003E52",
     textAlign: "left",
