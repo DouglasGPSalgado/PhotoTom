d24154ac99c380929efe58380d97e132613a2edd
Matheus Saito <matheussaito95k@gmail.com>
2023-03-02 17:12:40 -0300

    passagem de dados entre telas do form

--- photo-tom/src/screens/Form/Form4.js
+++ photo-tom/src/screens/Form/Form4.js
@@ -1,124 +1,144 @@
-import { useState } from 'react'
 import {
-  StyleSheet,
-  View,
-  ScrollView,
-  Image,
-  ImageBackground,
-  TouchableOpacity,
-  Alert,
-} from 'react-native'
-import { NativeBaseProvider, Box, Center, Text, Pressable } from 'native-base'
-import { globalStyles } from '../../GlobalStyles'
-import NavigationButton from '../../components/NavigationButton'
+    React,
+    useState
+} from "react";
+import {
+    StyleSheet,
+    View,
+    ScrollView,
+    Image,
+    ImageBackground,
+    TouchableOpacity,
+    Alert
+} from "react-native";
+import {
+    NativeBaseProvider,
+    Box,
+    Center,
+    Text,
+    Pressable
+} from "native-base";
+import { globalStyles } from "../../GlobalStyles";
+import NavigationButton from "../../components/NavigationButton";
 
-export default function Form4({ navigation }) {
-  const [quantidadeSardas, setQuantidadeSardas] = useState(null)
-  const validacao = () =>
-    Alert.alert('Ops', 'Selecione uma das alternativas para continuar!', [
-      { text: 'OK', onPress: () => console.log('OK Pressed') },
-    ])
+export default function Form4({navigation, route}) {
 
-  return (
-    <NativeBaseProvider>
-      <ScrollView>
-        <ImageBackground
-          source={require('@assets/Background.png')}
-          style={{ flex: 1 }}
-          resizeMode="cover"
-        >
-          <Image
-            source={require('@assets/image-form.png')}
-            style={{ height: 200, width: 200, alignSelf: 'center' }}
-            resizeMode="contain"
-          />
+    const [quantidadeSardas, setQuantidadeSardas] = useState(null);
+    const validacao = () =>
+        Alert.alert(
+            'Ops',
+            'Selecione uma das alternativas para continuar!',
+            [
+                { text: 'OK', onPress: () => console.log('OK Pressed') },
+            ]
+        );
 
-          <Center>
-            <Text style={globalStyles.formTitle}>
-              Qual é a quantidade de sardas na pele em áreas que não estão
-              expostas?
-            </Text>
-            <Pressable
-              onPress={(quantidadeSardas) => setQuantidadeSardas(0)}
-              style={globalStyles.formSelect}
-              borderColor={quantidadeSardas == 0 ? '#1EA1CA' : '#FFFFFF'}
-              shadow="7"
-            >
-              <Text
-                style={globalStyles.formSelectTitle}
-                color={quantidadeSardas == 0 ? '#1EA1CA' : '#003E52'}
-              >
-                Muitas
-              </Text>
-            </Pressable>
+    return (
+        <NativeBaseProvider>
+            <ScrollView>
+                <ImageBackground
+                    source={require("../../../assets/Background.png")}
+                    style={{ flex: 1 }}
+                    resizeMode="cover"
+                >
+                    <Image
+                        source={require("../../../assets/image-form.png")}
+                        style={{ height: 200, width: 200, alignSelf: "center" }}
+                        resizeMode="contain"
+                    />
 
-            <Pressable
-              onPress={(quantidadeSardas) => setQuantidadeSardas(1)}
-              style={globalStyles.formSelect}
-              borderColor={quantidadeSardas == 1 ? '#1EA1CA' : '#FFFFFF'}
-              shadow="7"
-            >
-              <Text
-                style={globalStyles.formSelectTitle}
-                color={quantidadeSardas == 1 ? '#1EA1CA' : '#003E52'}
-              >
-                Várias
-              </Text>
-            </Pressable>
+                    <Center>
+                        <Text
+                            style={globalStyles.formTitle}
+                        >
+                            Qual é a quantidade de sardas na pele em áreas que não estão expostas?
+                        </Text>
+                        <Pressable
+                            onPress={(quantidadeSardas) => setQuantidadeSardas(0)}
+                            style={globalStyles.formSelect}
+                            borderColor={quantidadeSardas == 0 ? "#1EA1CA" : "#FFFFFF"}
+                            shadow="7"
+                        >
+                            <Text
+                                style={globalStyles.formSelectTitle}
+                                color={quantidadeSardas == 0 ? "#1EA1CA" : "#003E52"}
+                            >
+                                Muitas
+                            </Text>
+                        </Pressable>
 
-            <Pressable
-              onPress={(quantidadeSardas) => setQuantidadeSardas(2)}
-              style={globalStyles.formSelect}
-              borderColor={quantidadeSardas == 2 ? '#1EA1CA' : '#FFFFFF'}
-              shadow="7"
-            >
-              <Text
-                style={globalStyles.formSelectTitle}
-                color={quantidadeSardas == 2 ? '#1EA1CA' : '#003E52'}
-              >
-                Algumas
-              </Text>
-            </Pressable>
+                        <Pressable
+                            onPress={(quantidadeSardas) => setQuantidadeSardas(1)}
+                            style={globalStyles.formSelect}
+                            borderColor={quantidadeSardas == 1 ? "#1EA1CA" : "#FFFFFF"}
+                            shadow="7"
+                        >
+                            <Text
+                                style={globalStyles.formSelectTitle}
+                                color={quantidadeSardas == 1 ? "#1EA1CA" : "#003E52"}
+                            >
+                                Várias
+                            </Text>
+                        </Pressable>
 
-            <Pressable
-              onPress={(quantidadeSardas) => setQuantidadeSardas(3)}
-              style={globalStyles.formSelect}
-              borderColor={quantidadeSardas == 3 ? '#1EA1CA' : '#FFFFFF'}
-              shadow="7"
-            >
-              <Text
-                style={globalStyles.formSelectTitle}
-                color={quantidadeSardas == 3 ? '#1EA1CA' : '#003E52'}
-              >
-                Muito poucas
-              </Text>
-            </Pressable>
+                        <Pressable
+                            onPress={(quantidadeSardas) => setQuantidadeSardas(2)}
+                            style={globalStyles.formSelect}
+                            borderColor={quantidadeSardas == 2 ? "#1EA1CA" : "#FFFFFF"}
+                            shadow="7"
+                        >
+                            <Text
+                                style={globalStyles.formSelectTitle}
+                                color={quantidadeSardas == 2 ? "#1EA1CA" : "#003E52"}
+                            >
+                                Algumas
+                            </Text>
+                        </Pressable>
 
-            <Pressable
-              onPress={(quantidadeSardas) => setQuantidadeSardas(4)}
-              style={globalStyles.formSelect}
-              borderColor={quantidadeSardas == 4 ? '#1EA1CA' : '#FFFFFF'}
-              shadow="7"
-            >
-              <Text
-                style={globalStyles.formSelectTitle}
-                color={quantidadeSardas == 4 ? '#1EA1CA' : '#003E52'}
-              >
-                Nenhuma
-              </Text>
-            </Pressable>
+                        <Pressable
+                            onPress={(quantidadeSardas) => setQuantidadeSardas(3)}
+                            style={globalStyles.formSelect}
+                            borderColor={quantidadeSardas == 3 ? "#1EA1CA" : "#FFFFFF"}
+                            shadow="7"
+                        >
+                            <Text
+                                style={globalStyles.formSelectTitle}
+                                color={quantidadeSardas == 3 ? "#1EA1CA" : "#003E52"}
+                            >
+                                Muito poucas
+                            </Text>
+                        </Pressable>
 
-            {quantidadeSardas == null ? (
-              <NavigationButton onPress={validacao} titulo={'Continuar'} />
-            ) : (
-              <NavigationButton
-                onPress={() => navigation.navigate('form5')}
-                titulo={'Continuar'}
-              />
-            )}
-          </Center>
-        </ImageBackground>
-      </ScrollView>
-    </NativeBaseProvider>
-  )
-}
+                        <Pressable
+                            onPress={(quantidadeSardas) => setQuantidadeSardas(4)}
+                            style={globalStyles.formSelect}
+                            borderColor={quantidadeSardas == 4 ? "#1EA1CA" : "#FFFFFF"}
+                            shadow="7"
+                        >
+                            <Text
+                                style={globalStyles.formSelectTitle}
+                                color={quantidadeSardas == 4 ? "#1EA1CA" : "#003E52"}
+                            >
+                                Nenhuma
+                            </Text>
+                        </Pressable>
+
+                        {
+                            quantidadeSardas == null ? (
+                                <NavigationButton
+                                    onPress={validacao}
+                                    titulo={"Continuar"}
+                                />
+                            ) : 
+                            <NavigationButton
+                                onPress={() => navigation.navigate("Form5")}
+                                titulo={"Continuar"}
+                            />
+                        }
+
+                    </Center>
+                </ImageBackground>
+            </ScrollView>
+        </NativeBaseProvider>
+    );
+};
--- photo-tom/src/screens/Form/Form5.js
+++ photo-tom/src/screens/Form/Form5.js
@@ -1,123 +1,144 @@
-import { useState } from 'react'
 import {
-  StyleSheet,
-  View,
-  ScrollView,
-  Image,
-  ImageBackground,
-  TouchableOpacity,
-  Alert,
-} from 'react-native'
-import { NativeBaseProvider, Box, Center, Text, Pressable } from 'native-base'
-import { globalStyles } from '../../GlobalStyles'
-import NavigationButton from '../../components/NavigationButton'
+    React,
+    useState
+} from "react";
+import {
+    StyleSheet,
+    View,
+    ScrollView,
+    Image,
+    ImageBackground,
+    TouchableOpacity,
+    Alert
+} from "react-native";
+import {
+    NativeBaseProvider,
+    Box,
+    Center,
+    Text,
+    Pressable
+} from "native-base";
+import { globalStyles } from "../../GlobalStyles";
+import NavigationButton from "../../components/NavigationButton";
 
-export default function Form5({ navigation }) {
-  const [bronze, setBonze] = useState(null)
-  const validacao = () =>
-    Alert.alert('Ops', 'Selecione uma das alternativas para continuar!', [
-      { text: 'OK', onPress: () => console.log('OK Pressed') },
-    ])
+export default function Form5({navigation, route}) {
 
-  return (
-    <NativeBaseProvider>
-      <ScrollView>
-        <ImageBackground
-          source={require('@assets/Background.png')}
-          style={{ flex: 1 }}
-          resizeMode="cover"
-        >
-          <Image
-            source={require('@assets/image-form.png')}
-            style={{ height: 200, width: 200, alignSelf: 'center' }}
-            resizeMode="contain"
-          />
+    const [bronze, setBonze] = useState(null);
+    const validacao = () =>
+        Alert.alert(
+            'Ops',
+            'Selecione uma das alternativas para continuar!',
+            [
+                { text: 'OK', onPress: () => console.log('OK Pressed') },
+            ]
+        );
 
-          <Center>
-            <Text style={globalStyles.formTitle}>
-              A pele do paciente fica bronzeada?
-            </Text>
-            <Pressable
-              onPress={(bronze) => setBonze(0)}
-              style={globalStyles.formSelect}
-              borderColor={bronze == 0 ? '#1EA1CA' : '#FFFFFF'}
-              shadow="7"
-            >
-              <Text
-                style={globalStyles.formSelectTitle}
-                color={bronze == 0 ? '#1EA1CA' : '#003E52'}
-              >
-                Nunca
-              </Text>
-            </Pressable>
+    return (
+        <NativeBaseProvider>
+            <ScrollView>
+                <ImageBackground
+                    source={require("../../../assets/Background.png")}
+                    style={{ flex: 1 }}
+                    resizeMode="cover"
+                >
+                    <Image
+                        source={require("../../../assets/image-form.png")}
+                        style={{ height: 200, width: 200, alignSelf: "center" }}
+                        resizeMode="contain"
+                    />
 
-            <Pressable
-              onPress={(bronze) => setBonze(1)}
-              style={globalStyles.formSelect}
-              borderColor={bronze == 1 ? '#1EA1CA' : '#FFFFFF'}
-              shadow="7"
-            >
-              <Text
-                style={globalStyles.formSelectTitle}
-                color={bronze == 1 ? '#1EA1CA' : '#003E52'}
-              >
-                Raramente
-              </Text>
-            </Pressable>
+                    <Center>
+                        <Text
+                            style={globalStyles.formTitle}
+                        >
+                            A pele do paciente fica bronzeada?
+                        </Text>
+                        <Pressable
+                            onPress={(bronze) => setBonze(0)}
+                            style={globalStyles.formSelect}
+                            borderColor={bronze == 0 ? "#1EA1CA" : "#FFFFFF"}
+                            shadow="7"
+                        >
+                            <Text
+                                style={globalStyles.formSelectTitle}
+                                color={bronze == 0 ? "#1EA1CA" : "#003E52"}
+                            >
+                                Nunca
+                            </Text>
+                        </Pressable>
 
-            <Pressable
-              onPress={(bronze) => setBonze(2)}
-              style={globalStyles.formSelect}
-              borderColor={bronze == 2 ? '#1EA1CA' : '#FFFFFF'}
-              shadow="7"
-            >
-              <Text
-                style={globalStyles.formSelectTitle}
-                color={bronze == 2 ? '#1EA1CA' : '#003E52'}
-              >
-                Às vezes
-              </Text>
-            </Pressable>
+                        <Pressable
+                            onPress={(bronze) => setBonze(1)}
+                            style={globalStyles.formSelect}
+                            borderColor={bronze == 1 ? "#1EA1CA" : "#FFFFFF"}
+                            shadow="7"
+                        >
+                            <Text
+                                style={globalStyles.formSelectTitle}
+                                color={bronze == 1 ? "#1EA1CA" : "#003E52"}
+                            >
+                                Raramente
+                            </Text>
+                        </Pressable>
 
-            <Pressable
-              onPress={(bronze) => setBonze(3)}
-              style={globalStyles.formSelect}
-              borderColor={bronze == 3 ? '#1EA1CA' : '#FFFFFF'}
-              shadow="7"
-            >
-              <Text
-                style={globalStyles.formSelectTitle}
-                color={bronze == 3 ? '#1EA1CA' : '#003E52'}
-              >
-                Frequentemente
-              </Text>
-            </Pressable>
+                        <Pressable
+                            onPress={(bronze) => setBonze(2)}
+                            style={globalStyles.formSelect}
+                            borderColor={bronze == 2 ? "#1EA1CA" : "#FFFFFF"}
+                            shadow="7"
+                        >
+                            <Text
+                                style={globalStyles.formSelectTitle}
+                                color={bronze == 2 ? "#1EA1CA" : "#003E52"}
+                            >
+                                Às vezes
+                            </Text>
+                        </Pressable>
 
-            <Pressable
-              onPress={(bronze) => setBonze(4)}
-              style={globalStyles.formSelect}
-              borderColor={bronze == 4 ? '#1EA1CA' : '#FFFFFF'}
-              shadow="7"
-            >
-              <Text
-                style={globalStyles.formSelectTitle}
-                color={bronze == 4 ? '#1EA1CA' : '#003E52'}
-              >
-                Sempre
-              </Text>
-            </Pressable>
+                        <Pressable
+                            onPress={(bronze) => setBonze(3)}
+                            style={globalStyles.formSelect}
+                            borderColor={bronze == 3 ? "#1EA1CA" : "#FFFFFF"}
+                            shadow="7"
+                        >
+                            <Text
+                                style={globalStyles.formSelectTitle}
+                                color={bronze == 3 ? "#1EA1CA" : "#003E52"}
+                            >
+                                Frequentemente
+                            </Text>
+                        </Pressable>
 
-            {bronze == null ? (
-              <NavigationButton onPress={validacao} titulo={'Continuar'} />
-            ) : (
-              <NavigationButton
-                onPress={() => navigation.navigate('form6')}
-                titulo={'Continuar'}
-              />
-            )}
-          </Center>
-        </ImageBackground>
-      </ScrollView>
-    </NativeBaseProvider>
-  )
-}
+                        <Pressable
+                            onPress={(bronze) => setBonze(4)}
+                            style={globalStyles.formSelect}
+                            borderColor={bronze == 4 ? "#1EA1CA" : "#FFFFFF"}
+                            shadow="7"
+                        >
+                            <Text
+                                style={globalStyles.formSelectTitle}
+                                color={bronze == 4 ? "#1EA1CA" : "#003E52"}
+                            >
+                                Sempre
+                            </Text>
+                        </Pressable>
+
+                        {
+                            bronze == null ? (
+                                <NavigationButton
+                                    onPress={validacao}
+                                    titulo={"Continuar"}
+                                />
+                            ) : 
+                            <NavigationButton
+                                onPress={() => navigation.navigate("Form6")}
+                                titulo={"Continuar"}
+                            />
+                        }
+
+                    </Center>
+                </ImageBackground>
+            </ScrollView>
+        </NativeBaseProvider>
+    );
+};
